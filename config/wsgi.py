8b6f46f2c282de420fe14d4dc183b4d04e20ddff
Osama <osamahashed6@gmail.com>
2025-12-20 18:10:29 +0300

    Fix WhiteNoise import in wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,7 +2,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise import WhiteNoise 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
